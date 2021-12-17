9b90198aea7d45395f129154fc3ea255640b1ec1
JasonWesleyAndrews <85419439+JasonWesleyAndrews@users.noreply.github.com>
2021-12-16 19:53:02 -0500

    created energy.gov case study page

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@ $("#mobileNav").on("click", function() {
     if ( $(".subNav").first().is( ":hidden" ) ) {
       $(".subNav").slideDown("slow");
       $(".grayBG").fadeIn("slow");
+      document.getElementById("mobileNav").innerHTML = "X";
+      
   
     
       
     } else {
       $(".subNav").slideUp("slow");
       $(".grayBG").fadeOut("slow");
-      
+      document.getElementById("mobileNav").innerHTML = "Menu";
     }
   });
 
